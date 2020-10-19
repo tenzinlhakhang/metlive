85312a35f51416d3670c6cb5769b9f6a7d0d9662
Tenzin Lhakhang <tenzin.c.lhakhang@gmail.com>
2020-10-19 12:28:28 -0400

    Update rdsconnect.py

--- rdsconnect.py
+++ rdsconnect.py
@@ -1,14 +1,1 @@
 
-import psycopg2
-
-
-
-engine = psycopg2.connect(
-    database="postgres",
-    user="Tenzin",
-    password="tenzin2014",
-    host="database-3.cmo9ouin7goc.us-east-1.rds.amazonaws.com",
-    port='5432'
-)
-
-
